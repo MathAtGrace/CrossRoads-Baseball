6a962de01857cbeb1c927ea536acd0746ab9a0c8
Kara Godsey <kjgodsey2@gmail.com>
2020-02-13 13:25:36 -0500

    Pitching league averages added, imported team schedules for park effects

--- R Scripts/Kara.R
+++ R Scripts/Kara.R
@@ -2,18 +2,61 @@ library(rvest)
 #require(tidyverse)
 
 load("Data/stats_by_type.Rdata")
-batting <- baseball[["Batting"]]
-pitching <- baseball[["Pitching"]]
-fielding <- baseball[["Fielding"]]
+Batting <- baseball[["Batting"]]
+Pitching <- baseball[["Pitching"]]
+Fielding <- baseball[["Fielding"]]
 teams <- names(batting)
 
+
 pitch_stats <- function(y){
-  x <- pitching[[y]]
+  x <- Pitching[[y]]
   #FIP (still need to add constant)
   x$FIP <- ((13*x$HR)+3*(x$BB+x$HBP)-(2*x$SO))/(x$IP)
   x
 }
 
-pitching <- teams %>%
+Pitching <- teams %>%
   lapply(pitch_stats)
-names(pitching) <- teams
+names(Pitching) <- teams
+
+#Calculate League Average for FIP
+
+FIP_stats <- c("ERA", "HR", "SO", "HBP", "BB", "IP")
+CL_Avg_P <- c()
+All_stats <- c()
+
+w = 0
+
+for (n in FIP_stats){
+  
+  for (j in teams){
+    
+    P <- read.csv(paste("Data/Pitching/",j,".csv",sep=""))
+    
+    
+    for (k in seq(length(P[[n]]))){ 
+      
+      All_stats[w+k] <- P[[n]][k]
+      #print(All_stats)
+    }
+    
+    w  = k + w
+    
+  }
+  CL_Avg_P[n] <- mean(All_stats)
+  All_stats <- c()
+  w = 0
+}
+
+print(CL_Avg_P)
+
+#get data for park effects
+p_effects <- read_html("http://www.dakstats.com/WebSync/Pages/Team/TeamSchedule.aspx?association=10&sg=MBA&sea=NAIMBA_2019&team=1679")
+
+tbls_effects <- p_effects %>%
+  html_nodes("table") %>%
+  .[36] %>%
+  html_table(fill = TRUE)
+
+Team_Schedule <- data.frame(tbls_effects[[1]])
+Team_Schedule <- Team_Schedule[!apply(is.na(Team_Schedule) | Team_Schedule == "", 1, all),]
