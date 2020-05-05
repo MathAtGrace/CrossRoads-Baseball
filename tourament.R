79d462026e83ddc9a4875bda5f10c38185ff8ae4
levi445 <63564753+levi445@users.noreply.github.com>
2020-05-04 18:21:51 -0700

    Added tournament simulation to main project website

--- tourament.R
+++ tourament.R
@@ -6,7 +6,7 @@ adjustment <- c()
 #adjustment for coaches poll (function can be changed)
 for (counter in ranks){
   
-  adjust_value <- counter*-2
+  adjust_value <- counter*-1
   adjustment[counter] <- adjust_value
   #print(adjustment)
 }
@@ -59,8 +59,8 @@ for (games in 1:nrow(tourney_matchups_randomized)){
   Home_tourney_temp <- tourney_matchups_randomized[games,1]
   Away_tourney_temp <- tourney_matchups_randomized[games,2]
   #get and adjust elo values
-  Home_tourney_temp_elo <- tourney_elos[toString(Home_tourney_temp)] + rnorm(1, mean=0, sd=25)#+ coaches_poll[toString(Home_tourney_temp), "adjustment"]
-  Away_tourney_temp_elo <- tourney_elos[toString(Away_tourney_temp)] + rnorm(1, mean=0, sd=25)#+ coaches_poll[toString(Away_tourney_temp), "adjustment"]
+  Home_tourney_temp_elo <- tourney_elos[toString(Home_tourney_temp)] + rnorm(1, mean=1+coaches_poll[Home_tourney_temp,]$adjustment, sd=25)#+ coaches_poll[toString(Home_tourney_temp), "adjustment"]
+  Away_tourney_temp_elo <- tourney_elos[toString(Away_tourney_temp)] + rnorm(1, mean=1+coaches_poll[Away_tourney_temp,]$adjustment, sd=25)#+ coaches_poll[toString(Away_tourney_temp), "adjustment"]
   
   #add elos to vector for dataframe
   Home_elos[games] <- Home_tourney_temp_elo
@@ -86,8 +86,8 @@ for (games in 1:nrow(tourney_matchups_randomized)){
   
   
   
-  Home_team_pred_score <- round(10*game_probs)
-  Away_team_pred_score <- round(10*(1-game_probs))
+  Home_team_pred_score <- 10*game_probs
+  Away_team_pred_score <- 10*(1-game_probs)
   
   #caculate new elo values based on predicted score
   run_elo_sim <- elo.calc(win_check, Home_tourney_temp_elo, Away_tourney_temp_elo, k(20*log(abs(Home_team_pred_score - Away_team_pred_score) + 1)))
@@ -110,6 +110,22 @@ for (games in 1:nrow(tourney_matchups_randomized)){
 tourney_matchups_randomized <- cbind(tourney_matchups_randomized, Home_elos, Away_elos, tourney_probs, winners)
 
 tourney_elos <- sort(tourney_elos,decreasing = TRUE)
+
+
+tourney_results_vec <- c()
+for (sim_x in (1:length(team_ranks))){
+  #print(team_ranks[sim_x])
+  games_won_sim <- length(winners[winners==team_ranks[sim_x]])
+  games_lost_sim <- 9-games_won_sim
+  #print(games_lost_sim)
+  sim_record <- paste(games_won_sim,games_lost_sim, sep="-")
+  tourney_results_vec[sim_x] <- sim_record
+  #print(tourney_results_vec)
+  
+  
+}
+
+tourney_results <- data.frame(team_ranks, tourney_results_vec)
 
 #tail(tourney_elos, n=1)
 
